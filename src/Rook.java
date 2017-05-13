1bde837c2773199d3025b4c77f3f996ad4033d54
Oscar Bjorkman <objorkman@live.com>
2017-05-12 18:13:01 -0700

    Added some rook stuff

--- src/Rook.java
+++ src/Rook.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 /**
  * Created by Oscar on 5/12/17.
  */
-public class Rook {
+public class Rook extends Piece {
+
+    public Rook(String id, String pictureName, int color) {
+        setId(id);
+        setPictureLocation(pictureName);
+        setColor(color);
+    }
+
+    @Override
+    public ArrayList<Cell> move(Cell[][] pos, int x, int y) {
+//        TODO: Override this
+    }
 }
+
